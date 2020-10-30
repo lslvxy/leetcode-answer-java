e6cbbe224043d4287c9b84032ff4f92d585af6d9
wb-ls691446 <wb-ls691446@antgroup.com>
2020-10-30 23:34:33 +0800

    update

---
+++ Solution0002.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 
+ * 示例：
+ * 
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4) 输出：7 -> 0 -> 8 原因：342 + 465 = 807
+ * 
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ * @author lise
+ * @since 2020-10-30 22:53:37
+ */
+public class Solution0002 {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = null, temp = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (result == null) {
+                result = temp = new ListNode(sum % 10);
+            } else {
+                temp.next = new ListNode(sum % 10);
+                temp = temp.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            temp.next = new ListNode(carry);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        System.out.println(addTwoNumbers(l1, l2));
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        str += val + "";
+        ListNode n = next;
+        if (n != null) {
+            str += next.toString();
+            n = next.next;
+        }
+        return str;
+    }
+}
